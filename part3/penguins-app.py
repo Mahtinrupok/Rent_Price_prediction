b9c5715c8583b9f0e5f87b5a552233e809c43be7
Mahtinrupok <82091449+Mahtinrupok@users.noreply.github.com>
2021-07-30 15:10:36 +0600

    Update penguins-app.py

--- part3/penguins-app.py
+++ part3/penguins-app.py
@@ -5,19 +5,12 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
-# Penguin Prediction App
-
-This app predicts the **Palmer Penguin** species!
-
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+# Rent Price Prediction Calculator
 """)
 
 st.sidebar.header('User Input Features')
 
-st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
-""")
-
+ 
 # Collects user input features into dataframe
 uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
 if uploaded_file is not None:
